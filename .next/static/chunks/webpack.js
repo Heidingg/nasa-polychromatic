aa19c5b6c80f24b272d07335c8914f1aac57696b
Heidingg <tnguyen480@my.bcit.ca>
2023-02-19 14:17:12 -0800

    changed text color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6806243188c54363"; }
+/******/ 		__webpack_require__.h = function() { return "ed209bba2f0e1e42"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
